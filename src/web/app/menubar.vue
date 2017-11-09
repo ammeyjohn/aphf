84d7ff4550601f0a04c15f2b06d5c7f59bb97c99
yuanjie <yuanjie@shanghai3h.com>
2017-11-09 14:20:00 +0800

    [Feature] Set actual menu name.

--- src/web/app/menubar.vue
+++ src/web/app/menubar.vue
@@ -12,82 +12,86 @@
             <!-- BEGIN NAVIGATION -->
             <div class="header-navigation pull-right font-transform-inherit">
                 <ul>
-                    <li class="dropdown active">
+                    <li class="open">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            首页
                         </a>
-
+                    </li>                    
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
+                            活动&公告
+                        </a>
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">活动主旨</a></li>
+                            <li><a href="index.html">2018APHF大会主席欢迎词</a></li>
+                            <li><a href="index.html">APHF亚太口琴节沿革</a></li>
+                            <li><a href="index.html">亚太口琴节筹委会记录</a></li>
+                            <li><a href="index.html">人员招募</a></li>
+                            <li><a href="index.html">音乐会DVD</a></li>
                         </ul>
                     </li>                    
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            比赛&赛程表
                         </a>
-
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">赛程表</a></li>
+                            <li><a href="index.html">比赛规则</a></li>
+                            <li><a href="index.html">秩序册</a></li>
+                            <li><a href="index.html">决赛入围名单</a></li>
+                            <li><a href="index.html">成绩查询</a></li>
                         </ul>
                     </li> 
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            线上报名
                         </a>
-
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">线上报名注意事项</a></li>
+                            <li><a href="index.html">个人报名</a></li>
+                            <li><a href="index.html">团体报名</a></li>
+                            <li><a href="index.html">查询资料</a></li>
+                            <li><a href="index.html">报名费用</a></li>
                         </ul>
                     </li> 
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            节目&贵宾
                         </a>
-
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">节目表</a></li>
+                            <li><a href="index.html">音乐会</a></li>
+                            <li><a href="index.html">管选乐团编制</a></li>
+                            <li><a href="index.html">评委介绍 I</a></li>
+                            <li><a href="index.html">评委介绍 II</a></li>
+                            <li><a href="index.html">指定曲作曲者</a></li>
                         </ul>
                     </li> 
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            场地&酒店资讯
                         </a>
-
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">会场交通资讯</a></li>
+                            <li><a href="index.html">大会场馆资讯</a></li>
+                            <li><a href="index.html">大会指定酒店</a></li>
+                            <li><a href="index.html">大会推荐酒店一览表</a></li>
+                            <li><a href="index.html">大会推荐旅游行程</a></li>
                         </ul>
                     </li> 
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            合作伙伴
                         </a>
-
                         <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
+                            <li><a href="index.html">赞助者</a></li>
+                            <li><a href="index.html">主协办单位</a></li>                            
                         </ul>
                     </li> 
-                    <li class="dropdown">
+                    <li>
                         <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
-                            Home                
+                            联络方式         
                         </a>
-
-                        <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Default</a></li>
-                            <li class="active"><a href="index-header-fix.html">Home with Header Fixed</a></li>
-                            <li><a href="index-without-topbar.html">Home without Top Bar</a></li>
-                        </ul>
                     </li>                                                                                                                         
                 </ul>
             </div>
