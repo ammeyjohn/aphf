ebaa702b059436af27e36f30b32a964a16cbcedf
Patrick <ammeyjohn@gmail.com>
2017-11-10 21:53:15 +0800

    [Feature] Add aphf.api project wirtten by .net core 2.0

---
+++ src/server/APHF.Models/AuditLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace APHF.Models {
+    /// <summary>
+    /// 定义审计日志
+    /// </summary>
+    public class AuditLog {
+
+        /// <summary>
+        /// 获取或设置IP地址
+        /// </summary>
+        public string IpAddress { get; set; }
+
+        /// <summary>
+        /// 获取或设置URL地址
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 获取或设置日志内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 获取或设置日志详细内容
+        /// </summary>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 获取或设置日志记录时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
